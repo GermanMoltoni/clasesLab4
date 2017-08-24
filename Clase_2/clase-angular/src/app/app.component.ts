d8c35aebd0d10d4949a15f392ec5275192d3eab7
alumno <alumno>
2017-08-24 20:41:41 -0300

    Componente nuevo y spinner

--- Clase_2/clase-angular/src/app/app.component.ts
+++ Clase_2/clase-angular/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Clase Angular';
+  // Instalar bootstrap y modificara .angular-cli.json para incluir el archivo
+  //ng g component componentes/login
+  // para agregar imagenes poner en assets de .angular-cli.json
 }
