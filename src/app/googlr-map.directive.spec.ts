f542e01e7e8ccf61490a4273c16edbd897c3dac8
Marcelo <programacion@informaticasa.com.ar>
2017-11-21 17:28:15 -0300

    cambios jwt

--- src/app/googlr-map.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GooglrMapDirective } from './googlr-map.directive';
-
-describe('GooglrMapDirective', () => {
-  it('should create an instance', () => {
-    const directive = new GooglrMapDirective();
-    expect(directive).toBeTruthy();
-  });
-});
